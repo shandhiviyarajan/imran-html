019a692496309f2e417c141dc2899eed7a8c7fd9
Shan Dhiviyarajan <prashasoft@gmail.com>
2021-05-07 09:58:26 +0530

    slider,search fix

--- public/js/app.js
+++ public/js/app.js
@@ -87,6 +87,10 @@
                       el: ".swiper-pagination",
                       clickable: true,
                     },
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                      }
                   });
             }
 
